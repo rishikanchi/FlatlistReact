c0b860f9775f77b1cfc1e756ca71808d7a9e4841
Rishi Kanchi <98680362+rishikanchi@users.noreply.github.com>
2025-01-29 11:01:42 -0600

    Created flatlist

--- flatlistApp/app/index.tsx
+++ flatlistApp/app/index.tsx
@@ -7,8 +7,27 @@ import {
 } from "react-native";
 import colors from "../styles/colors";
 import defaultStyles from "../styles/defaultStyles";
+import { useState } from "react";
 
 export default function Index() {
+
+  type dataType = {
+    id: string,
+    title: string,
+  }
+  const DATA: dataType[] = [
+    {id: '1', title: 'First'},
+    {id: '2', title: 'Second'},
+    {id: '3', title: 'Third'},
+    {id: '4', title: 'Fourth'},
+  ];
+  const selectedList = (item: dataType) => {
+    setSelectedId(item.id);
+    console.log("Clicked on " + item.title);
+  }
+
+  const [selectedId, setSelectedId] = useState<string>("1")
+
   return (
     <View style={defaultStyles.container}>
       <View style={defaultStyles.titleContainer}>
@@ -16,7 +35,25 @@ export default function Index() {
       </View>
       <View style={[defaultStyles.textContainer, { flex: 1 }]}>
         <View style={styles.flatlist}>
-          <Text>This is where our list will go</Text>
+          <FlatList 
+            data={DATA}
+            keyExtractor={(item: dataType) => item.id}
+            renderItem={({ item }) => (
+              <TouchableOpacity onPress={() => selectedList(item)}>
+                <View style={[styles.titleContainer, {
+                  backgroundColor: 
+                    item.id === selectedId 
+                      ? colors.primary 
+                      : colors.secondary
+                },
+                ]}>
+                  <Text style={[styles.titleText, {color: item.id === selectedId ? colors.text.light : colors.text.dark}]}>
+                    {item.title}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            )}
+          />
         </View>
       </View>
     </View>
@@ -32,6 +69,10 @@ const styles = StyleSheet.create({
     width: 300,
     borderTopLeftRadius: 15,
     borderTopRightRadius: 15,
+    backgroundColor: 'lightblue'
+  },
+  selectedContainer: {
+    backgroundColor: 'blue'
   },
   titleText: {
     fontSize: 24,
